7ba66f03fa180320440ed3cf9cf597bc50e94c07
飯田白米@Win <hakumai@garnet.broba.cc>
2020-03-03 23:49:26 +0900

    サンプル手順書に合わせたファイルに置き換え（コメントの内容が変わったのみ）

--- app/src/main/java/com/sample/studyweb3j/MainActivity.java
+++ app/src/main/java/com/sample/studyweb3j/MainActivity.java
@@ -2,30 +2,39 @@ package com.sample.studyweb3j;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+//import android.util.Log;
 import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
-    // テストクラスのインスタンス
+    //- 追加 テストクラスのインスタンス ----
     private TestWeb3 testWeb3;
+    //--------------------------------------
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-        // ECDSAアルゴリズムを利用するための設定
+    protected void onCreate(Bundle savedInstanceState) {
+        //- 追加 ECDSAアルゴリズムを利用するための設定 ----
         Web3Helper.SetupBouncyCastle();
+        //-------------------------------------------------
 
-        // テストインスタンスの作成
+        //- 追加 テストインスタンスの作成 -----------------
         testWeb3 = new TestWeb3( this );
+        //-------------------------------------------------
 
         super.onCreate(savedInstanceState);
     }
 
+    //- 追加 タッチイベント：押下されたらテスト処理を呼び出す -------
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        // 画面がタップされたらテストを実行する
         if( event.getAction() == MotionEvent.ACTION_DOWN ){
+            // ここでテストを呼び出す
+            //Log.i( "StudyWeb3j", "@ DO SOMETHING!!" );
+
+            // テストの呼び出し
             testWeb3.test();
         }
 
         return super.onTouchEvent(event);
     }
+    //---------------------------------------------------------------
 }
